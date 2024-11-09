874ec2fc79eed3f9d7b592548f30e155d58da199
Greco S Lagunas <167014931+MkGLSN@users.noreply.github.com>
2024-11-08 19:57:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//Com esse poucos comandos vamos dar "sentido" ao botão.
+//É por isso que la no HTML tinha um entrar()no comando botão.
+//Para encaminhar para outro site, é só colocar esses comandos : function entrar(){window.location.href = 'https://codepen.io/lucas-pereira-the-sans'}
+//Mas vocês terão que editar o link que eu coloquei no exemplo, no meu caso está encaminhando para o meu perfil, mas vocês peguem o link da página que queren que seja encaminhado e coloquem.
+// Pronto é isso, se gostou curta o projeto. Click no botão e entre no meu perfil e veja outros projetos que podem te ajudar, Obrigado.
+
+function entrar() {
+  window.location.href = 'https://codepen.io/lucas-pereira-the-sans'
+}
